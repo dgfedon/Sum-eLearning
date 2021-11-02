0ee9c2cf0fa138a2051e9140f444e82f759c01e6
Dariana Garcia <dgarciafedon@gamil.com>
2021-11-02 15:29:51 -0300

    actualización secciones

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+
+         autoplay:true,
+         autoplayhoverpause: true,
+         autoplaytimeout:100,
+         items:3.5,
+         nav: true,
+         loop:true,
+      })
+   });
+
+
+   // alert("EL SCRIPT ANDA")
